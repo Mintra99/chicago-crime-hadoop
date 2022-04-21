eed4f61edf31cf2454b829e0bd3cc044b47d83d6
Torhaca <tor.hakon@laposte.net>
2022-04-21 15:55:54 +0200

    worked on spark

--- run_job_spark.py
+++ run_job_spark.py
@@ -1,58 +1,19 @@
 import math
-import os
-from typing import Counter
-from unittest import skip
-# os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
-# os.environ["SPARK_HOME"] = "/content/spark-2.4.5-bin-hadoop2.7"
-
-import sys
-sys.setrecursionlimit(1000)
-
 import findspark
-from matplotlib import pyplot as plt
-import numpy as np
-
-findspark.init()
-from pyspark.sql import SparkSession
-spark = SparkSession.builder.master("local[*]").getOrCreate()
 
 # spark imports
-from pyspark.sql import SparkSession
 from pyspark.sql.types import *
-from pyspark.sql.functions import monotonically_increasing_id
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
 
 
-# Date;Block;Primary Type;Description;Arrest;Domestic;Beat;Community Area;Year;Updated On;Location
 crimes_schema = StructType([StructField("ID", IntegerType(), True),
-                            # StructField("FBI Code", StringType(), True ),
                             StructField("Date", StringType(), True ),
-                            # StructField("Block", StringType(), True),
                             StructField("Primary Type", StringType(), True  ),
-                            # StructField("Description", StringType(), True ),
-                            # StructField("Location Description", StringType(), True ),
-                            # StructField("Arrest", BooleanType(), True),
-                            # StructField("Domestic", BooleanType(), True),
-                            # StructField("Beat", IntegerType(), True),
-                            # StructField("District", IntegerType(), True),
-                            # StructField("Ward", IntegerType(), True),
-                            # StructField("Community Area", IntegerType(), True),
-                            # 
-                            # StructField("X Coordinate", IntegerType(), True),
-                            # StructField("Y Coordinate", IntegerType(), True ),
-                            # StructField("Year", IntegerType(), True),
-                            # StructField("Updated On", StringType(), True ),
                             StructField("Latitude", DoubleType(), True),
                             StructField("Longitude", DoubleType(), True),
                             StructField("Location", StringType(), True )
                             ])
-
-
-# dataset = spark.sparkContext.textFile("hdfs://DAT500:8020/spark_preprocess.csv")
-dataset = spark.read.csv('spark_preprocess.csv', header = True,schema = crimes_schema)
-dataset = dataset.drop("ID")
-
-location = dataset.limit(150000).select("Location")
-#location = dataset.select("Location")
 
 class spark_kmeans():
     def __init__(self):
@@ -68,13 +29,10 @@ class spark_kmeans():
                            [41.846664648, -87.617318718],
                            [41.954345702, -87.726412567]]
         
-        self.X = []
-        self.Y = []
         
     def fit(self):
         self.convert_coords()
-        self.create_coord_df()
-        # self.kmeans()
+        self.kmeans()
 
         
     def convert_coords(self):
@@ -85,14 +43,6 @@ class spark_kmeans():
             y_coord = float(new_coords[1])
             coords = [x_coord, y_coord]
             self.coords.append(coords)
-    
-    def create_coord_df(self):
-        col_header = ["X_coord", "Y_coord"]            
-        df = spark.createDataFrame(data=self.coords, schema = col_header)
-        self.coord_df = df.withColumn("id", monotonically_increasing_id())
-        for point in self.coords:
-            self.X.append(point[0])
-            self.Y.append(point[1])
     
     def getDistance(self, centroid, point):
         cur_dist = math.sqrt((centroid[0]-point[0])**2 + (centroid[0]-point[0])**2) 
@@ -139,15 +89,100 @@ class spark_kmeans():
             self.x_list.append(centroid[0])
             self.y_list.append(centroid[1])
             print(centroid)
-        
-    def plot(self):
-        plt.scatter(self.X, self.Y, c=self.prediction)
-        plt.scatter(self.x_list, self.y_list, c='red')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.show()
+
+
+class WRCentroids():
+    def retrieveCentroids(self, file):
+        with open(file, "r") as inputFile:
+            output_data = inputFile.readlines()
+        centroids = []
+        for line in output_data:
+            line = line.split(';')
+            untreated_str_coords = line[1]
+            str_coords = ''.join(c for c in untreated_str_coords if c not in '[ ]')
+            coords = str_coords.split(',')
+            try:
+                x_coord = float(coords[0].strip())
+            except ValueError:
+                x_coord = float(0)
             
+            try:
+                y_coord = float(coords[1].strip())
+            except ValueError:
+                y_coord = float(0)
+            
+            new_coord = [x_coord, y_coord]
+
+            if len(new_coord) == 2:
+                centroids.append(new_coord)
+        return centroids
+
+    def writeCentroids(self, centroids, file):
+        f = open(file, "w+")
+        iteration = 1
+        for coord in centroids:
+            string_item = str(iteration) + ' ; ' + str(coord) + ' ; 1'
+            f.write("%s\n" % string_item)
+            iteration += 1
+        f.close()
+    
+    def badWrite(self, centroids, file):
+        f = open(file, "w+")
+        for item in centroids:
+            string_item = ''.join(c for c in str(item) if c not in "[]")
+            f.write("%s\n" % string_item)
+        f.close()
+    
+    def checkCloseness(a, b, rel_tol=1e-05, abs_tol=0.0):
+        return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
+
 if __name__ == "__main__":
+    """install_requires=[
+        'pyspark=={site.SPARK_VERSION}'
+    ]"""
+
+    wrCentroid = WRCentroids()
+
+    conf = SparkConf().setAppName("PySPark CrimesChicago").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
+    findspark.init()
+    spark = SparkSession.builder.master("local[*]").getOrCreate()
+
+    distFile = sc.textFile("data.txt")
+
+    rdd = sc.textFile("hdfs://spark_preprocess.csv")
+
+    #
+    distFile.map(lambda s: len(s)).reduce(lambda a, b: a + b).
+
+    i = 1 # + " --centroids=" \ # mellom data og files
+    while True:
+        distFile.map(lambda s: len(s)).reduce(lambda a, b: a + b)
+
+        new_centroids = wrCentroid.retrieveCentroids(output)
+        print("NEW CENTROIDS: " + str(new_centroids))
+
+        # """
+        min_dist = 0.0001
+        done = True
+        for i in range(len(new_centroids)):
+            distance = math.sqrt(pow(centroids[i][0]-new_centroids[i][0], 2) + pow(centroids[i][1] - new_centroids[i][1], 2)) 
+            if distance > min_dist:
+                done = False
+
+        if done:
+            break
+        else:
+            centroids = new_centroids
+            if len(centroids) != 0:
+                wrCentroid.writeCentroids(centroids, file)
+        # """
+        
+        i +=1
+
     kmeans = spark_kmeans()
     kmeans.fit()
-    kmeans.plot()
+
+    # https://spark.apache.org/docs/latest/rdd-programming-guide.html
